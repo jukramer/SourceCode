1c11a1581213da660cd953d3e05bb8f53c42eb82
jukramer <justas.kramer@gmail.com>
2025-04-27 20:23:07 +0200

    updated

--- simulator.cpp
+++ simulator.cpp
@@ -2,22 +2,5 @@
 #include "API.h"
 
 int main() {
-    // Disable stdout and stderr buffering for real-time I/O
-    setvbuf(stdout, nullptr, _IONBF, 0);
-    setvbuf(stderr, nullptr, _IONBF, 0);
-
-    // Handshake: read maze size
-    int W = API::mazeWidth();
-    int H = API::mazeHeight();
-
-    // Simple wall-following loop
-    while (true) {
-        if (!API::wallFront()) {
-            API::moveForward();
-        } else {
-            API::turnRight();
-        }
-    }
-
-    return 0;
+     mazeWidth();
 }
