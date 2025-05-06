3e36345d9320ff043417518611bf49d5517b4c21
jukramer <justas.kramer@gmail.com>
2025-05-06 14:47:26 +0200

    almost finished floodfill

--- SourceCode.cpp
+++ SourceCode.cpp
@@ -6,6 +6,9 @@
 #include <queue>
 #include <stack>
 #include "functions.h"
+#include <chrono>
+#include <thread>
+#include <iomanip>
 // #include "pico/stdlib.h"
 // #include "pico/cyw43_arch.h"
 // #include "pins.hpp"
@@ -57,14 +60,22 @@ class Mouse
         int floodMatrix[16][16];
 
         Mouse() {
+            // Initialize mazeMatrix
+            for (int i=0; i<16; i++) {
+                for (int j=0; j<16; j++) {
+                    mazeMatrix[i][j]={0,0,0,0,0};
+                }
+            }
+
+            // Initialize floodMatrix
             for (int i=0; i<n; i++) {
                 for (int j=0; j<n; j++) {
-                    floodMatrix[i][j] = manhattanDist({i, j}, {{7,7}, {7,8}, {8,7}, {8,8}});
-                    std::cout << floodMatrix[i][j] << " ";
+                    floodMatrix[i][j] = 255;
+                    // std::cout << floodMatrix[i][j] << " ";
                 }
-                std::cout << std::endl;
+                // std::cout << std::endl;
             }
-            std::cout << floodMatrix[8][8];
+            // std::cout << floodMatrix[8][8];
         }
 };
 
@@ -79,7 +90,7 @@ std::vector<int> cellConfig(Mouse mouse)
     double tl, tr, tf; // threshold values for front, left, and right
     std::vector<double> dVec = {dl, df, dr};
     std::vector<double> tVec = {tl, tf, tr};
-    std::vector<int> cellVec = {0, 0, 0, 0}; // contains value for wall in each cell, and whether it has been checked: left, front, right, bottom, checked (y/n). 1=wall, 0=no wall
+    std::vector<int> cellVec = {0, 0, 0, 0}; // left, top, right, bottom, checked (y/n). 1=wall, 0=no wall
 
     // places wall around cells if detected 
     for (int i=0; i <= dVec.size(); i++) {
@@ -186,31 +197,45 @@ void mapping(Mouse mouse) // handle overall movement of the mouse
     }
 }
 
-std::vector<int> getNeighbors(std::vector<int> cell) 
+
+std::vector<std::vector<int>> getNeighbors(std::vector<int> cell) 
 {
     std::vector<std::vector<int>> neighbors = {};
-    int x0 = cell[0], int y0 = cell[1];
-    int x1 = x0-1, int y1 = y0; // left cell
-    if (x1 !< 0 && x1 !> 16 && y1 !< 0 && y1 !> 16) {
+    int x0 = cell[0], y0 = cell[1];
+    int x1 = x0-1, y1 = y0; // left cell
+    if (x1 >= 0 && x1 <= 16 && y1 >= 0 && y1 <= 16) {
         neighbors.push_back({x1, y1});
     }
 
-    int x2 = x0, int y2 = y0 + 1; // top cell
-    if (x2 !< 0 && x2 !> 16 && y2 !< 0 && y2 !> 16) {
+    int x2 = x0, y2 = y0 + 1; // top cell
+    if (x2 >= 0 && x2 <= 16 && y2 >= 0 && y2 <= 16) {
         neighbors.push_back({x2, y2});
     }
 
-    int x3 = x0+1, int y3 = y0; // right cell
-    if (x3 !< 0 && x3 !> 16 && y3 !< 0 && y3 !> 16) {
+    int x3 = x0+1, y3 = y0; // right cell
+    if (x3 >= 0 && x3 <= 16 && y3 >= 0 && y3 <= 16) {
         neighbors.push_back({x3, y3});
     }
 
-    int x4 = x0, int y4 = y0-1; // bottom cell
-    if (x4 !< 0 && x4 !> 16 && y4 !< 0 && y4 !> 16) {
+    int x4 = x0, y4 = y0-1; // bottom cell
+    if (x4 >= 0 && x4 <= 16 && y4 >= 0 && y4 <= 16) {
         neighbors.push_back({x4, y4});
     }
 
     return neighbors;
+}
+
+
+void printFloodfill(Mouse mouse)
+{
+    // std::cout << "hello!!" << std::endl; 
+    for (int i=0; i<16; i++) {
+        for (int j=0; j<16; j++) {
+            std::cout << std::setw(4)<< mouse.floodMatrix[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+    // std::cout << floodMatrix[8][8];
 }
 
 
@@ -241,20 +266,43 @@ int main()
     // stdio_init_all();
 
     // main loop
-    gpio_put(ledPin, 1);
-    while (true) {
-        printf("LED on!");
-        // gpio_put(ledPin, 1);
-        // sleep_ms(500);
-        printf("LED off!");
-        // gpio_put(ledPin, 0);
-        // sleep_ms(500);
-    }
+    // gpio_put(ledPin, 1);
+
     
+    // floodfill
+    // std::cout << mouse.solvingType << std::endl; 
     if (mouse.solvingType == 0) {
-        x0 = mouse.cellPos[0];
-        y0 = mouse.cellPos[1];
-        mouse.mazeMatrix[x0][y0] = cellConfig(mouse);
+        auto start = std::chrono::high_resolution_clock::now();
+        // reset floodfill map
+        for (int i=0; i<16; i++) {
+            for (int j=0; j<16; j++) {
+                mouse.floodMatrix[i][j] = 255;
+                // std::cout << floodMatrix[i][j] << " ";
+            }
+            // std::cout << std::endl;
+        }
+        mouse.floodMatrix[7][7] = 0;
+        mouse.floodMatrix[7][8] = 0;
+        mouse.floodMatrix[8][7] = 0;
+        mouse.floodMatrix[8][8] = 0;
+
+        // std::cout << "hello" << std::endl;
+        int x0 = mouse.cellPos[0];
+        int y0 = mouse.cellPos[1];
+        // mouse.mazeMatrix[x0][y0] = cellConfig(mouse);
+        // mouse.mazeMatrix[3][3] = {1,1,0,0,1};
+        mouse.mazeMatrix[13][4] = {0,1,1,0,1};
+        mouse.mazeMatrix[6][5] = {0,0,0,1,1};
+        mouse.mazeMatrix[6][6] = {0,0,0,1,1};
+        mouse.mazeMatrix[6][7] = {0,0,0,1,1};
+        mouse.mazeMatrix[6][8] = {0,0,0,1,1};
+        mouse.mazeMatrix[6][9] = {0,0,0,1,1};
+        mouse.mazeMatrix[6][10] = {0,0,0,1,1};
+        // mouse.mazeMatrix[7][7] = {1,0,0,1,1};
+        // mouse.mazeMatrix[8][7] = {1,1,0,0,1};
+        // mouse.mazeMatrix[7][8] = {0,0,1,1,1};
+        // mouse.mazeMatrix[8][8] = {0,1,1,0,1};
+
         std::queue<std::vector<int>> floodQueue;
         
         // Add goal cells to queue
@@ -262,12 +310,60 @@ int main()
         floodQueue.push({7,8});
         floodQueue.push({8,7});
         floodQueue.push({8,8});
+        int c = 0;
 
         while (floodQueue.size() != 0) {
-            currentCell = floodQueue.front()
-            
+            std::vector<int> currentCell = floodQueue.front();
+            floodQueue.pop();
+            c++;
+            int xc = currentCell[0], yc = currentCell[1];
+            std::vector<std::vector<int>> neighbors = getNeighbors(currentCell);
+            // std::cout << floodQueue.size()<<" - "<<xc<<" "<<yc<<std::endl;
+            for (int i=0; i<4; i++) {
+                    try {
+                        // std::cout << neighbor[0] << " " << neighbor[1] << " " << std::endl;
+                        int xn = neighbors[i][0], yn = neighbors[i][1];
+                        // std::cout<<"ello"<<std::endl;
+                        // std::cout<<"-------------------"<<std::endl;
+                        // std::cout<<i<<" ";
+                        // std::cout<<mouse.mazeMatrix[yc][xc][0]<<" ";
+                        // std::cout<<mouse.floodMatrix[yn][xn]<<" ";
+                        // std::cout<<mouse.floodMatrix[yc][xc]<<" ";
+                        // std::cout<<"Checked: "<<xn<<" "<<yn<<" with value "<<mouse.floodMatrix[yn][xn]<<std::endl;
+                        if (i==0 && mouse.mazeMatrix[yc][xc][0]==0 && mouse.floodMatrix[yn][xn] > (mouse.floodMatrix[yc][xc]+1)) {
+                            mouse.floodMatrix[yn][xn] = mouse.floodMatrix[yc][xc]+1;
+                            floodQueue.push(neighbors[i]);
+                            // std::cout<<"Pushed "<<xn<<" "<<yn<<std::endl;
+                        } if (i==1 && mouse.mazeMatrix[yc][xc][1]==0 && mouse.floodMatrix[yn][xn] > (mouse.floodMatrix[yc][xc]+1)) {
+                            mouse.floodMatrix[yn][xn] = mouse.floodMatrix[yc][xc]+1;
+                            floodQueue.push(neighbors[i]);
+                            // std::cout<<"Pushed "<<xn<<" "<<yn<<std::endl;
+                        } if (i==2 && mouse.mazeMatrix[yc][xc][2]==0 && mouse.floodMatrix[yn][xn] > (mouse.floodMatrix[yc][xc]+1)) {
+                            mouse.floodMatrix[yn][xn] = mouse.floodMatrix[yc][xc]+1;
+                            floodQueue.push(neighbors[i]);
+                            // std::cout<<"Pushed "<<xn<<" "<<yn<<std::endl;
+
+                        } if (i==3 && mouse.mazeMatrix[yc][xc][3]==0 && mouse.floodMatrix[yn][xn] > (mouse.floodMatrix[yc][xc]+1)) {
+                            mouse.floodMatrix[yn][xn] = mouse.floodMatrix[yc][xc]+1;
+                            floodQueue.push(neighbors[i]);
+                            // std::cout<<"Pushed "<<xn<<" "<<yn<<std::endl;
+                        } 
+                        // std::cout<<"ello"<<std::endl;
+                    }
+                    catch (...) {
+                        ;
+                    }
+                }
+            }
+            // std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+            auto end = std::chrono::high_resolution_clock::now();
+            std::chrono::duration<double, std::milli> duration = end-start;
+            printFloodfill(mouse);
+            std::cout<<"Checked "<<c<<" cells in "<<duration.count()<<" ms."<<std::endl;
         }
-        std::cin.get();
-    }
+        // std::cout<<"hellohello"<<std::endl;
+        
+    
+    // std::cin.get();
 }
 
